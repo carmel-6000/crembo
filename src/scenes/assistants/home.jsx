dfe2c8c442036e50a4cf0671bf0d1877722f1219
Shira <shiraadascohen100@gmail.com>
2019-04-16 10:18:18 +0300

    proton

--- src/scenes/assistants/home.jsx
+++ src/scenes/assistants/home.jsx
@@ -6,42 +6,31 @@ class AssistantSide extends Component {
     constructor(props){
         super(props);
         this.state = {
-            isInclude: null
-        }
         
-        this.gettingRideData();
+    }
+}
 
+    setPhone = (event) => {
+        this.setState({phone: event.target.value})
     }
 
   enterForm = () => {
       return (
       <form>
-          <label><input type="number"></input> מספר פלאפון</label>
-          <select>
-              <option value="forward">הלוך</option>
-              <option value="back">חזור</option>
-          </select>
-<<<<<<< HEAD
-          <Link to={{pathname: "/assistant/3682/ride-details", state:{watched: this.state.watched} }}><button>כניסה למערכת</button></Link>
-=======
-          <Link to="/assistant/ride-details"><button>כניסה למערכת</button></Link>
->>>>>>> 27b649bf62357e169a56cda4c83eef5323bbaf01
+          <label>
+              <input type="number" onChange={this.setPhone }>
+              </input>
+               מספר פלאפון
+              </label>
+          <Link to={{pathname: "/assistant/rides", state:{watched: this.state.watched, phone: this.state.phone} }}>
+          <button disabled={!this.state.phone}>כניסה למערכת</button>
+          </Link>
       </form>)
   }
 
-  gettingRideData = () => {
-   Auth.authFetch('/api/rides?filter={ 	"where": {"activities": {"isLive": true}     } , 	"include": ["assistants", "activities"] }').then(response => { return response.json() }).then(res => {
-        console.log("answer of",'/api/rides?filter={{"where": {"activities": {"isLive": true}     }}{ "include": ["assistants", "activities"]}}');
-        console.log("res",res);
-        this.setState({ isInclude: res })
-    })    
-}
-  
-  
   
   
     render() {
-        console.log("rides" , this.state.isInclude)
         return(this.enterForm())
     }
 }
